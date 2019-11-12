7079ab6834fc3da66e31e7dbbe7b9f38e5b2486b
hyunus <hyunus@uwaterloo.ca>
2019-11-12 00:05:52 -0500

    implemented google chart with hardcoded input data

--- src/app/gamedata/gamedata.component.ts
+++ src/app/gamedata/gamedata.component.ts
@@ -7,11 +7,21 @@ import { ApiService } from '../api.service';
   styleUrls: ['./gamedata.component.css']
 })
 export class GamedataComponent implements OnInit {
+  //retrieved data properties
   gameData;
+
+  //google chart properties
+  title = 'Accuracy';
+  type = 'ColumnChart';
+  data = [['A', 81.76286090720873], ['B', 82.24383116883116], ['C', 82.58051948051947]] ;
+  width = 800;
+  height = 400;
+
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    //get data from backend
     this.apiService.getGameData().subscribe((data)=>{
       console.log(data);
       this.gameData = data['records'];
