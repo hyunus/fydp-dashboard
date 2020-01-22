388e243fb11dc1bcf5220790aaf44db5d317bb0e
hyunus <hyunus@edu.uwaterloo.ca>
2020-01-21 22:10:07 -0500

    completed add patient view

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-patients',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class PatientsComponent implements OnInit {
 
   user: object={};
+  searchForm: FormGroup;
+  query: string="";
 
-  constructor() { 
+  addView: boolean=false;
+  //form properties
+  patientForm: FormGroup;
+  firstName: string="";
+  lastName: string="";
+  email: string="";
+  parentEmail: string="";
+  conditions: string="";
+  goals: string=""
+
+  constructor(private fb: FormBuilder) { 
     this.user = JSON.parse(localStorage.getItem('currentUser'));
+    this.searchForm = fb.group({
+      'query': [null]
+    })
+    this.patientForm = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.email],
+      'parentEmail': [null, Validators.compose([Validators.required, Validators.email])],
+      'conditions': [null, Validators.required],
+      'goals': [null, Validators.required]
+    })
+   }
+
+   toggleView() {
+    this.addView = !this.addView;
+   }
+
+   addPatient(form:NgForm) {
+     //placeholder for posting the form to the patient API
+     console.log(form);
+     this.toggleView();
    }
 
   ngOnInit() {
