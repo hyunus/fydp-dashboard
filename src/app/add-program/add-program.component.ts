4724f1ba401ada31c9c57abaa7daeafdb87db028
hyunus <hyunus@edu.uwaterloo.ca>
2020-02-27 15:31:44 -0500

    implemented horizontal scrolling game list with metadata filtering

--- src/app/add-program/add-program.component.ts
+++ src/app/add-program/add-program.component.ts
@@ -5,13 +5,15 @@ import { ApiService } from '../_services/api.service';
 @Component({
   selector: 'app-add-program',
   templateUrl: './add-program.component.html',
-  styleUrls: ['./add-program.component.css', '../home/home.component.css', '../profile/profile.component.css']
+  styleUrls: ['./add-program.component.css', '../home/home.component.css', '../profile/profile.component.css', '../games/games.component.css', '../patients/patients.component.css']
 })
 export class AddProgramComponent implements OnInit {
 
   user: Object;
   patient: string;
   profile: {};
+  gameList: Array<Object>;
+  query: string="";
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
@@ -22,16 +24,23 @@ export class AddProgramComponent implements OnInit {
     this.route.queryParams
     .subscribe(params => {
       this.patient = params['id'];
-      console.log(this.user['code'], this.patient)
     }, (error) => {
       console.log(error)
     })
 
+    //get patient profile
     this.apiService.getProfile(this.user['code'], this.patient).subscribe((response) => {
       this.profile = response['records'][0];
-      console.log(this.profile)
     }, (error) => {
       console.log(error);
+    })
+
+    //get game list
+    this.apiService.getGamelist().subscribe((response) => {
+      this.gameList = response['records']
+      console.log(this.gameList)
+    }, (error) => {
+      console.log(error)
     })
   }
 
