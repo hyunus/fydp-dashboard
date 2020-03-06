569ac0f827854c5415ffb82945e2d6c185b0cc67
hyunus <hyunus@edu.uwaterloo.ca>
2020-03-05 19:27:32 -0500

    home page content update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,16 +15,32 @@ export class HomeComponent implements OnInit {
     //hard-coded article list for testing purposes
     this.articles = [
       {
-        title: "See how Mightier is using video games to teach kids emotion calming skills",
+        title: "How Mightier is using video games to teach kids emotion calming skills",
         subtitle: "Teaching Kids Self-Regulation Through Play",
         link: "MIGHTIER GAMES",
+        url: "/",
         img: "/assets/article1.png"
       },
       {
         title: "See Pluto Care's new outpatient adherence tracking features",
         subtitle: "Timeline, Performance, and Homework",
         link: "OUTPATIENT ADHERENCE",
+        url: "/#patients",
         img: "/assets/article2.png"
+      },
+      {
+        title: "NEW GAME: Rec Room for environment organization",
+        subtitle: "Rec Room has partnered with Pluto Care to release an occupational therapy version of their titular VR game Rec Room. This features multiple rooms for exploration, organization, and fun!",
+        link: "REC ROOM",
+        url: "/",
+        img: "/assets/article3.png"
+      },
+      {
+        title: "Hand VR Exergame for Occupational Health Care",
+        subtitle: "Researchers develop a hand motion-based virtual reality-based exergame for children to prevent some of the muskoloskeletal problems associated with the over-use of keyboards and mobile devices.",
+        link: "ONTARIO TECH UNIVERSITY",
+        url: "/",
+        img: "/assets/article4.png"
       }
     ]
    }
